937147b3db8be79ee32b16171467467073c82436
fillias <filous.doma@volny.cz>
2018-06-27 12:35:23 +0200

    mini weather react app

--- app/index.js
+++ app/index.js
@@ -1,1 +1,50 @@
-document.write('hi webpacku fff');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const api_key = '37c1e40dd8fb2b4986ec2384ca2786a1';
+
+class App extends Component {
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			city: 'Cagliari',
+			country: '',
+			description: '',
+			temp: ''
+		}
+	}
+
+	componentDidMount() {
+		this.grapWeather(this.state.city);
+	}
+
+	grapWeather(city) {
+		fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&units=metric&q=${city}`)
+			.then(response => {
+				console.log('fetch response: ', response);
+				return response.json();
+			})
+			.then( json => {
+				console.log('json response: ', json);
+				this.setState({description: json.weather[0].description});
+				this.setState({temp: json.main.temp});
+				this.setState({country: json.sys.country});
+			})
+			
+	}
+
+
+	render() {
+		return (
+			<div>
+				<h1>report pro: {this.state.city}, {this.state.country}</h1>
+				<h2>{this.state.description}</h2>
+				<h2>{this.state.temp}</h2>
+			</div>
+		)
+		
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-build prostredi s webpack a babelem
+build prostredi s webpack a babelem a react, weather app
 
 webpack buildi dev i produkci, viz package.json
 babel transpiluje do es5
@@ -7,7 +7,7 @@ babel transpiluje do es5
 pouziti:
 
 	npm run start 
-	- spusti dev server na portu 300 co servri obsah adresare 'build'. Kouka to do adresare 'app' a bere js file odtamtad. S kazdym save reloadne server i aplikaci
+	- spusti dev server na portu 3000 co servri obsah adresare 'build'. Kouka to do adresare 'app' a bere js file odtamtad. S kazdym save reloadne server i aplikaci
 
 	npm run build
 	- zbuildi aplikaci, transpiluje do es5, minifikuje a ulozi jako bundle.js v adresari build
